ad396a3faede0172ee0b2086df5645096f1c88e4
Alex Menendez <amenendez15@usf.edu>
2025-09-24 15:21:52 -0400

    forgot to commit lol

--- cc_5.js
+++ cc_5.js
@@ -1,3 +1,5 @@
+//Step 2
+
 let employees = 
 [ 
 {name: "Bruno Mars", hourlyRate: 17, hoursWorked: 40},
@@ -6,10 +8,46 @@ let employees =
 ];
 
 
- function calculateBasePay(hourlyRate,hoursWorked){
-    console.log(`Hourly Rate is ${hourlyRate}`);
-    return hourlyRate * Math.max(hoursWorked,40)
+//Step 3
+
+function calculateBasePay(rate,hours){
+    return rate * Math.min(hours,40);
 }
-function calculateOvertimePay(hourlyRate,hoursWorked){
-return 1.5 * hourlyRate * Math.min(hoursWorked,40);
+
+//Step 4
+
+function calculateOvertimePay(rate, hours) {
+return  Math.max(hours - 40, 0) * rate * 1.5;
+} 
+//Step 5
+function calculateTaxes(grossPay) {
+return grossPay * .15
 }
+
+//Step 6
+
+function processPayroll(employee){
+    const basePay = calculateBasePay(employee.hourlyRate, employee.hoursWorked)
+    const overtimePay = calculateOvertimePay(employee.hourlyRate, employee.hoursWorked)
+    const grossPay = basePay + overtimePay
+    const netPay = grossPay - calculateTaxes(grossPay)
+
+return {
+    name: employee.name,
+    basePay: basePay.toFixed(2),
+    overtimePay: overtimePay.toFixed(2),
+    grossPay: grossPay.toFixed(2),
+    netPay: netPay.toFixed(2)
+}
+}
+
+//Step 7 
+employees.forEach(employee => console.log(processPayroll(employee)));
+
+// for (const employee of employees) {
+//     console.log(processPayroll(employee))
+// }
+
+
+
+
